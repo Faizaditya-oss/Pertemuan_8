6efb9ddb37e9a59f57c124eb9d015fabf15825f6
Faizaditya-oss <faizaditya750@gmail.com>
2026-08-05 10:23:06 +0700

    menambahkan tahun terbit dan id penulis komik model

--- models/komik.js
+++ models/komik.js
@@ -13,3 +13,10 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.TEXT,
             allowNull: true
         },
+        tahun_terbit: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        penulis_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
